853f7424db79cc2c24d95ade261c40b92486d4ee
koide <koide@aisl.cs.tut.ac.jp>
2017-10-31 17:35:56 +0900

    initial commit

---
+++ src/pclomp/gicp_omp.cpp
@@ -0,0 +1,5 @@
+#include <pclomp/gicp_omp.h>
+#include <pclomp/gicp_omp_impl.hpp>
+
+template class pclomp::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>;
+template class pclomp::GeneralizedIterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI>;
