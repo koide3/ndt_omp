cf5ea870df1cf993aa840302ae7e9158d9bfcc15
marbosjo <mbosch@robotnik.es>
2019-08-19 15:25:55 +0200

    solved linking issue with pcl

--- include/pclomp/ndt_omp.h
+++ include/pclomp/ndt_omp.h
@@ -42,6 +42,7 @@
 #define PCL_REGISTRATION_NDT_OMP_H_
 
 #include <pcl/registration/registration.h>
+#include <pcl/search/impl/search.hpp>
 #include "voxel_grid_covariance_omp.h"
 
 #include <unsupported/Eigen/NonLinearOptimization>
